b71a518a6c28407cb2a6e1e5e810794e74176a11
RuiOkazaki <okazaki200226@gmail.com>
2023-03-05 19:39:19 +0900

    hook test

--- src/hooks/useInitialAnalytics.test.ts
+++ src/hooks/useInitialAnalytics.test.ts
@@ -7,6 +7,7 @@ const spiedSetStatus = jest.spyOn(analyticsModule, "setStatus")
 const spiedSendPageview = jest.spyOn(analyticsModule, "sendPageview")
 
 test("useInitialAnalytics 初回実行", () => {
+  // Hooksを呼び出すためにrenderHookを使う
   renderHook(() => useInitialAnalytics({ id: "foo", role: "bar" }))
   expect(spiedSetStatus).toBeCalledWith({ id: "foo", role: "bar" })
   expect(spiedSendPageview).toBeCalled()
